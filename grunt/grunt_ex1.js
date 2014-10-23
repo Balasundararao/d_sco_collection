0029e1cd1c48fabeb8cdcf371fc205b2d2876993
balasundar <bthunga@cisco.com>
2014-10-22 22:27:35 -0700

    updated file

--- grunt/grunt_ex1.js
+++ grunt/grunt_ex1.js
@@ -9,3 +9,45 @@ module.exports = function(grunt){
        grunt.log.writeln("Hello  "  + "--" +name +" --" + grunt.config.get("person").firstName);
     });
 };
+
+
+/**
+module.exports=function(grunt){
+
+//File1:
+//    grunt.initConfig({
+//        person:{
+//            firstName:"Frank"
+//        }
+//    })
+//    grunt.initConfig(grunt.file.readJSON("config.json"));
+//    grunt.registerTask("default", function(name){
+//        grunt.log.writeln("Hello  "  + "--" +name +" --" + grunt.config.get("person").firstName);
+//    });
+
+//File2:
+//    grunt.initConfig({
+//        asks:{
+//            one:"Hello",
+//            two:"TWO",
+//            three:"Three"
+//        }
+//    });
+//    grunt.registerMultiTask("asks",function(){
+//        grunt.log.writeln(this.target+ "---" +this.data);
+//    });
+
+//File3:
+    grunt.initConfig({
+        min:{
+            dist:{
+                src:'calculator/add.js',
+                dest:'add.min.js'
+
+            }
+        }
+    });
+//    grunt.registerMultiTask("asks",function(){
+//        grunt.log.writeln(this.target+ "---" +this.data);
+//    });
+}; **/
