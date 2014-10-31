ba6a893113d179fa48ab82cf4cce902e0026d725
balasundar <bthunga@cisco.com>
2014-10-31 08:28:21 -0700

    updated app.module.js file for model,style and teams

---
+++ angularjs/src/client/app/teams/teams.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+    angular
+        .module('app.ts')
+        .controller('teamsCtrl', teamsCtrl);
+	
+    function teamsCtrl() {
+    	/* jshint validthis: true */
+    	var ts = this;
+    }   // end function 
+})();
+
