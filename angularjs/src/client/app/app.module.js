f705e96bd6b1f69518f840b2d75ca98d44d03e0e
balasundar <bthunga@cisco.com>
2014-10-31 08:27:23 -0700

    updated app.module.js file for model,style and teams

--- angularjs/src/client/app/app.module.js
+++ angularjs/src/client/app/app.module.js
@@ -4,32 +4,31 @@
     	'app.dashboard',
         'app.smoketest',
         'app.cl',
-        'app.rwj',
+        'app.rdj',
+        'app.ar',
+        'app.mql',
+        'app.sg',
+        'app.ts',
+        'app.mo',
         'ngRoute',
         'ngResource'
     ]).constant('version', 'v0.1.0')
     .config(function ($locationProvider, $routeProvider) {
         $locationProvider.html5Mode(false);
         $routeProvider.when('/',                        {templateUrl: 'src/client/app/dashboard/dashboard.html'})
-        $routeProvider.when('/smoketest/:sandbox',      {templateUrl: 'src/client/app/smoke_test/smoketest.html',          controller: 'smokeTestCtrl'})
         $routeProvider.when('/componentlisting',        {templateUrl: 'src/client/app/cl_listing/componentlisting.html',   controller: 'clListCtrl'})
-        $routeProvider.when('/rd-json',                 {templateUrl: 'src/client/app/rd-json/rd-json.html',               controller: 'rwjsonCtrl'})
-        $routeProvider.when('/agentreport',             {templateUrl: 'views/agents.html',                controller: 'agentController'})
-        //$routeProvider.when('/releaselistings',         {templateUrl: 'partials/releaselistings',        controller: 'relListCtrl'})
-        //$routeProvider.when('/citienvironmentlisting',  {templateUrl: 'partials/citienvironmentlisting', controller: 'envListCtrl'})    
-        //$routeProvider.when('/listing',               {templateUrl: 'views/ptiflisting.html'})
-        //$routeProvider.when('/listing_grid',          {templateUrl: 'views/ptiflisting_grid.html'})
+        $routeProvider.when('/rd-json',                 {templateUrl: 'src/client/app/rd-json/rd-json.html',               controller: 'rdjsonCtrl', controllerAs: 'rd',})
+        $routeProvider.when('/smoketest/:sandbox',      {templateUrl: 'src/client/app/smoke_test/smoketest.html',          controller: 'smokeTestCtrl'})
+        $routeProvider.when('/agentreport',             {templateUrl: 'src/client/app/agentreport/agentreport.html',       controller: 'agentreportCtrl', controllerAs:'ar'})
+        $routeProvider.when('/teams',        {templateUrl: 'src/client/app/teams/teams.html',   controller: 'teamsCtrl', controllerAs:'ts'})
+        $routeProvider.when('/mql',          {templateUrl: 'src/client/app/mql/mql.html',       controller: 'mqlCtrl', controllerAs:'mq'})
+        $routeProvider.when('/models',       {templateUrl: 'src/client/app/model/model.html',   controller: 'modelCtrl', controllerAs:'mo'})
+        $routeProvider.when('/style',        {templateUrl: 'src/client/app/style/style.html',   controller: 'styleCtrl', controllerAs:'sg'})
         .otherwise({redirectTo: '/'});
     });
 })();
 
-
-// MODULARIZED CODE...  
-// (function() {
-//     'use strict';
-//     angular.module('app', [
-//     	'app.dashboard',
-//         'app.smoketest',
-//         'app.cl'
-//     ]);
-// })();
+//$routeProvider.when('/releaselistings',         {templateUrl: 'partials/releaselistings',        controller: 'relListCtrl'})
+//$routeProvider.when('/citienvironmentlisting',  {templateUrl: 'partials/citienvironmentlisting', controller: 'envListCtrl'})    
+//$routeProvider.when('/listing',               {templateUrl: 'views/ptiflisting.html'})
+//$routeProvider.when('/listing_grid',          {templateUrl: 'views/ptiflisting_grid.html'})
