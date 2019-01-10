3a874665803e7cdfe36488fe3d45a0dff0619b91
Balasundrarao Thunga <bthunga@cisco.com>
2019-01-10 10:11:37 -0800

    Updated files

--- phanthomjs/chapter1_file2.js
+++
@@ -1,9 +0,0 @@
-var page = require("webpage").create();
-page.open("http://www.packtpub.com", function(status){
-	if( status === "success"){
-		console.log("Success:"+ page.title);
-	}else{
-		console.log("Page failed to load");
-	}
-	phantom.exit(0);
-});
--- phanthomjs/pageload.js
+++
@@ -1,5 +0,0 @@
-var page = require('webpage').create();
-page.open('http://www.cisco.com/', function () {
-    page.render('cisco.png');
-    phantom.exit();
-});
